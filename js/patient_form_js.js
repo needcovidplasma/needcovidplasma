d7e927c9a6870425d4cfff509027114a09ef8881
needcovidplasma <82691968+needcovidplasma@users.noreply.github.com>
2021-05-09 08:47:52 +0530

    Update patient_form_js.js

--- js/patient_form_js.js
+++ js/patient_form_js.js
@@ -228,7 +228,7 @@ var address = getInputVal('address');
 }
    
 
-const sm="notifications.needcovidplasma@gmail.com";
+const sm="plasmaneed@gmail.com";
 const volunteer_alert_mail_message="Need of plasma from recovered corona people has been requested for the following person";
 const end_volunteer_message="<br>Please circulate the above details in your known groups or social media pages thereby increasing the chance finding a donor<br>Please don't reply back to this mail<br><br>needcovidplasma.in";
 const pw="covidplasma1";
